d866407a6394fa8bf84b4c05808ff305b852ce00
zverkovavik <viktorya_zverkova@mail.ru>
2021-06-17 23:51:38 +0300

    Added leaflet map with ad markers

--- project/src/components/form-to-submit-comment.jsx
+++ project/src/components/form-to-submit-comment.jsx
@@ -1,60 +1,32 @@
 import React, { useState } from 'react';
+import { ratingForm } from '../constants';
+
+const createRatingForm = (numberValue, definition) => (
+  <React.Fragment key={definition}>
+    <input className="form__rating-input visually-hidden" name="rating" value={numberValue} id={`${numberValue}-stars`} type="radio" />
+    <label htmlFor={`${numberValue}-stars`} className="reviews__rating-label form__rating-label" title={definition}>
+      <svg className="form__star-image" width="37" height="33">
+        <use xlinkHref="#icon-star"></use>
+      </svg>
+    </label>
+  </React.Fragment>);
 
 function NewCommentForm(props) {
 
-  const [comment, setComment] = useState(null);
-  const [rating, setRating] = useState(null);
-  // используем свойства чтобы не ругался линтер
-  const commentToSubmit = {
-    comment: comment,
-    rating: rating,
-  };
+  const [newComment, setValue] = useState({ comment: null, rating: null });
 
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
-      <div  onClick={(evt) => setRating(evt.target.value)} className="reviews__rating-form form__rating" >
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
+      <div  onClick={(evt) => setValue({ ...newComment, rating: evt.target.value})} className="reviews__rating-form form__rating" >
+        {ratingForm.map((ratingFormElement) => createRatingForm(ratingFormElement.numberValue, ratingFormElement.definition))}
       </div>
-      <textarea onChange={(evt) => setComment(evt.target.value)} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+      <textarea onChange={(evt) => setValue({ ...newComment, comment: evt.target.value})} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
                       To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button onSubmit={(evt) => { evt.preventDefault(); return commentToSubmit; }} className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
+        <button onSubmit={(evt) => evt.preventDefault()} className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
       </div>
     </form>
   );
